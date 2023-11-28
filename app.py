f30ef9ed3039ad906e651c444a7b29e40c4efe9b
DhulipudiAchyutSaiKrishna <achyut.dhulipudi@gmail.com>
2023-11-28 10:10:43 +0530

    modified

--- app.py
+++ app.py
@@ -50,3 +50,56 @@ if __name__ == '__main__':
     app.config['UPLOAD_FOLDER'] = 'uploads'
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     app.run(debug=True)
+
+# from flask import Flask, render_template, request
+# from werkzeug.utils import secure_filename
+# import os
+# import pandas as pd
+# import json
+
+# app = Flask(__name__)
+
+# # Function to process uploaded CSV files
+# def process_uploaded_csv(file):
+#     if file and file.filename.endswith('.csv'):
+#         filename = secure_filename(file.filename)
+#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         return filename
+
+# # Function to process uploaded JSON files
+# def process_uploaded_json(file):
+#     if file and file.filename.endswith('.json'):
+#         filename = secure_filename(file.filename)
+#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         return filename
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         uploaded_file = request.files['file']
+#         if uploaded_file:
+#             filename = None
+#             if uploaded_file.filename.endswith('.csv'):
+#                 filename = process_uploaded_csv(uploaded_file)
+#             elif uploaded_file.filename.endswith('.json'):
+#                 filename = process_uploaded_json(uploaded_file)
+
+#             if filename:
+#                 # Read and display the data
+#                 file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#                 if filename.endswith('.csv'):
+#                     df = pd.read_csv(file_path)
+#                 elif filename.endswith('.json'):
+#                     with open(file_path, 'r') as json_file:
+#                         data = json.load(json_file)
+#                         df = pd.DataFrame(data)
+
+#                 # Display the data in a table
+#                 return render_template('index.html', tables=[df.to_html(classes='data', escape=False)])
+
+#     return render_template('index.html', tables=[])
+
+# if __name__ == '__main__':
+#     app.config['UPLOAD_FOLDER'] = 'uploads'
+#     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+#     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,12 +1,3 @@
-// // script.js
-// // function highlightText(text, search) {
-// //   // Use a regular expression to perform a global search and replace
-// //   var pattern = new RegExp(search, "gi");
-// //   return text.replace(pattern, '<span class="highlight">$&</span>');
-// // }
-
-// // modified version
-
 // function highlightText(text, search) {
 //   var pattern = new RegExp("(" + search + ")", "gi");
 //   return text.replace(pattern, '<span class="highlight">$1</span>');
@@ -18,80 +9,213 @@
 //   filter = input.value.toLowerCase();
 //   table = document.getElementsByClassName("data")[0];
 //   tr = table.getElementsByTagName("tr");
+//   var rowCount = 0; // Initialize row count to 0
 
-//   //   for (i = 1; i < tr.length; i++) {
-//   //     var rowVisible = false;
-//   //     for (j = 0; j < tr[i].cells.length; j++) {
-//   //       td = tr[i].cells[j];
-//   //       if (td) {
-//   //         txtValue = td.textContent || td.innerText;
-//   //         if (txtValue.toLowerCase().indexOf(filter) > -1) {
-//   //           rowVisible = true;
-//   //           break;
-//   //         }
-//   //       }
-//   //     }
-//   //     if (rowVisible) {
-//   //       tr[i].style.display = "";
-//   //     } else {
-//   //       tr[i].style.display = "none";
-//   //     }
-//   //   }
 //   for (i = 1; i < tr.length; i++) {
 //     var rowVisible = false;
 //     for (j = 0; j < tr[i].cells.length; j++) {
 //       td = tr[i].cells[j];
 //       if (td) {
 //         txtValue = td.textContent || td.innerText;
+//         var highlightedText = highlightText(txtValue, filter);
+//         td.innerHTML = highlightedText; // Highlight the text in the cell
 //         if (txtValue.toLowerCase().indexOf(filter) > -1) {
 //           rowVisible = true;
-//           break;
 //         }
-//         var highlightedText = highlightText(txtValue, filter);
-//         td.innerHTML = highlightedText;
 //       }
 //     }
 //     if (rowVisible) {
 //       tr[i].style.display = "";
+//       rowCount++; // Increment row count when a row is visible
 //     } else {
 //       tr[i].style.display = "none";
 //     }
 //   }
+
+//   // Update the row count display
+//   document.getElementById("rowCount").textContent = "Rows Found: " + rowCount;
 // }
+
+
+// var originalTableHTML = ""; // Variable to store the original table HTML
+
+// function highlightText(text, search) {
+//     var pattern = new RegExp("(" + search + ")", "gi");
+//     return text.replace(pattern, '<span class="highlight">$1</span>');
+// }
+
+// function filterTable() {
+//     var input, filter, table, tr, td, i, j, txtValue;
+//     input = document.getElementById("searchInput");
+//     filter = input.value.toLowerCase();
+//     table = document.getElementsByClassName("data")[0];
+//     tr = table.getElementsByTagName("tr");
+//     var rowCount = 0;
+
+//     for (i = 1; i < tr.length; i++) {
+//         var rowVisible = false;
+//         for (j = 0; j < tr[i].cells.length; j++) {
+//             td = tr[i].cells[j];
+//             if (td) {
+//                 txtValue = td.textContent || td.innerText;
+//                 var highlightedText = highlightText(txtValue, filter);
+//                 td.innerHTML = highlightedText;
+//                 if (txtValue.toLowerCase().indexOf(filter) > -1) {
+//                     rowVisible = true;
+//                 }
+//             }
+//         }
+//         if (rowVisible) {
+//             tr[i].style.display = "";
+//             rowCount++;
+//         } else {
+//             tr[i].style.display = "none";
+//         }
+//     }
+
+//     document.getElementById("rowCount").textContent = "Rows Found: " + rowCount;
+// }
+
+// function removeColumns() {
+//     var input, columnsToRemove;
+//     input = document.getElementById("removeColumnsInput");
+//     columnsToRemove = input.value.split(",").map(column => column.trim());
+
+//     if (originalTableHTML === "") {
+//         originalTableHTML = document.getElementsByClassName("data")[0].innerHTML;
+//     }
+
+//     var table = document.getElementsByClassName("data")[0];
+//     var tr = table.getElementsByTagName("tr");
+
+//     for (var i = 0; i < tr.length; i++) {
+//         for (var j = 0; j < columnsToRemove.length; j++) {
+//             var columnIndex = Array.from(tr[i].getElementsByTagName("th")).findIndex(th => th.textContent.trim() === columnsToRemove[j]);
+//             if (columnIndex !== -1) {
+//                 tr[i].deleteCell(columnIndex);
+//             }
+//         }
+//     }
+// }
+
+// function restoreColumns() {
+//     if (originalTableHTML !== "") {
+//         document.getElementsByClassName("data")[0].innerHTML = originalTableHTML;
+//     }
+// }
+
+
+// function downloadTable() {
+//   var table = document.getElementsByClassName("data")[0];
+//   var tableHTML = table.outerHTML;
+
+//   // Create a Blob containing the table HTML
+//   var blob = new Blob([tableHTML], { type: "text/html" });
+
+//   // Create a download link
+//   var a = document.createElement("a");
+//   a.href = URL.createObjectURL(blob);
+//   a.download = "table_data.html";
+
+//   // Append the link to the body and trigger the download
+//   document.body.appendChild(a);
+//   a.click();
+
+//   // Remove the link from the body
+//   document.body.removeChild(a);
+// }
+
+
+
+var originalTableHTML = ""; // Variable to store the original table HTML
+
 function highlightText(text, search) {
-  var pattern = new RegExp("(" + search + ")", "gi");
-  return text.replace(pattern, '<span class="highlight">$1</span>');
+    var pattern = new RegExp("(" + search + ")", "gi");
+    return text.replace(pattern, '<span class="highlight">$1</span>');
 }
 
 function filterTable() {
-  var input, filter, table, tr, td, i, j, txtValue;
-  input = document.getElementById("searchInput");
-  filter = input.value.toLowerCase();
-  table = document.getElementsByClassName("data")[0];
-  tr = table.getElementsByTagName("tr");
-  var rowCount = 0; // Initialize row count to 0
+    var input, filter, table, tr, td, i, j, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toLowerCase();
+    table = document.getElementsByClassName("data")[0];
+    tr = table.getElementsByTagName("tr");
+    var rowCount = 0;
 
-  for (i = 1; i < tr.length; i++) {
-    var rowVisible = false;
-    for (j = 0; j < tr[i].cells.length; j++) {
-      td = tr[i].cells[j];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        var highlightedText = highlightText(txtValue, filter);
-        td.innerHTML = highlightedText; // Highlight the text in the cell
-        if (txtValue.toLowerCase().indexOf(filter) > -1) {
-          rowVisible = true;
+    for (i = 1; i < tr.length; i++) {
+        var rowVisible = false;
+        for (j = 0; j < tr[i].cells.length; j++) {
+            td = tr[i].cells[j];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                var highlightedText = highlightText(txtValue, filter);
+                td.innerHTML = highlightedText;
+                if (txtValue.toLowerCase().indexOf(filter) > -1) {
+                    rowVisible = true;
+                }
+            }
         }
-      }
+        if (rowVisible) {
+            tr[i].style.display = "";
+            rowCount++;
+        } else {
+            tr[i].style.display = "none";
+        }
     }
-    if (rowVisible) {
-      tr[i].style.display = "";
-      rowCount++; // Increment row count when a row is visible
-    } else {
-      tr[i].style.display = "none";
-    }
-  }
 
-  // Update the row count display
-  document.getElementById("rowCount").textContent = "Rows Found: " + rowCount;
+    document.getElementById("rowCount").textContent = "Rows Found: " + rowCount;
+}
+
+function removeColumns() {
+    var input, columnsToRemove;
+    input = document.getElementById("removeColumnsInput");
+    columnsToRemove = input.value.split(",").map(column => column.trim());
+
+    if (originalTableHTML === "") {
+        originalTableHTML = document.getElementsByClassName("data")[0].innerHTML;
+    }
+
+    var table = document.getElementsByClassName("data")[0];
+    var tr = table.getElementsByTagName("tr");
+
+    for (var i = 0; i < tr.length; i++) {
+        for (var j = 0; j < columnsToRemove.length; j++) {
+            var columnIndex = Array.from(tr[i].getElementsByTagName("th")).findIndex(th => th.textContent.trim() === columnsToRemove[j]);
+            if (columnIndex !== -1) {
+                tr[i].deleteCell(columnIndex);
+            }
+        }
+    }
+
+    // After removing columns, update the search results
+    filterTable();
+}
+
+function restoreColumns() {
+    if (originalTableHTML !== "") {
+        document.getElementsByClassName("data")[0].innerHTML = originalTableHTML;
+
+        // After restoring columns, update the search results
+        filterTable();
+    }
+}
+
+function downloadTable() {
+    var table = document.getElementsByClassName("data")[0];
+    var tableHTML = table.outerHTML;
+
+    // Create a Blob containing the table HTML
+    var blob = new Blob([tableHTML], { type: "text/html" });
+
+    // Create a download link
+    var a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = "table_data.html";
+
+    // Append the link to the body and trigger the download
+    document.body.appendChild(a);
+    a.click();
+
+    // Remove the link from the body
+    document.body.removeChild(a);
 }
